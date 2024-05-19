38b005eb1a5592cecdfde9e01bd051a01e667e0b
vatari <majestic13@gmail.com>
2024-05-19 08:04:42 +0300

    lab 07

---
+++ data_Types_working_with_tables/lab/07.insert_data_in_tables.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, middle_name, hiring_date, salary, devices_number)
+VALUES
+    ('John', 'Doe', 'Smith', '2023-05-15', 50000.00, 3),
+    ('Jane', 'Smith', 'Doe', '2023-06-20', 60000.00, 2),
+    ('Michael', 'Johnson', NULL, '2023-07-10', 55000.00, 1);
