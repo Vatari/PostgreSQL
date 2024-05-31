c298b893e60fd6e020c43f2eba4ec3636ffea81d
vatari <majestic13@gmail.com>
2024-05-31 09:51:58 +0300

    exercise 18

---
+++ built_in_functions/exercise/18.arithmetical_operators.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS bookings_calculation
+AS
+SELECT
+	booked_for,
+	booked_for::"numeric" * 50 AS multiplication,
+	booked_for::"numeric" % 50 AS modulo
+FROM bookings
+WHERE apartment_id = 93;
