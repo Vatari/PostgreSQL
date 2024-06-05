0694f1b247a7e62de2588c6bc536c2120ab05279
vatari <majestic13@gmail.com>
2024-06-05 08:05:30 +0300

    exercise 17

---
+++ data_aggregation/exercise/17.having_salary_level.sql
@@ -0,0 +1,10 @@
+SELECT department_id,
+       count(*) AS num_employees,
+       CASE
+           WHEN avg(salary) > 50000 THEN 'Above average'
+           WHEN avg(salary) <= 50000 THEN 'Below average'
+           END AS salary_level
+FROM employees
+GROUP BY department_id
+HAVING avg(salary) > 30000
+ORDER BY department_id;
