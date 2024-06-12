7a43e0c756326ce8f937c0b3fe178df9640ed1dd
vatari <majestic13@gmail.com>
2024-06-12 07:52:55 +0300

    lab 03

---
+++ subqueries_and_JOINs/lab/03.employees_projects.sql
@@ -0,0 +1,8 @@
+SELECT e.employee_id,
+       concat_ws(' ', e.first_name, e.last_name) AS full_name,
+       p.project_id,
+       p.name                                    AS project_name
+FROM employees e
+         JOIN employees_projects ep USING (employee_id)
+         JOIN projects p USING (project_id)
+WHERE p.project_id = 1;
