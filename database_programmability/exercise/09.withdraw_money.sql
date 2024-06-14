d6188cfa5e5de1f4d9c875739089fe2b30380955
vatari <majestic13@gmail.com>
2024-06-14 08:47:11 +0300

    exercise 09

---
+++ database_programmability/exercise/09.withdraw_money.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PROCEDURE sp_withdraw_money(
+    account_id INT,
+    money_amount NUMERIC
+)
+AS
+$$
+DECLARE
+    account_balance NUMERIC;
+BEGIN
+    -- check if provided account_id exists
+    IF (SELECT balance FROM accounts WHERE id = account_id) IS NULL THEN
+        RETURN;
+    ELSE
+        account_balance := (SELECT balance FROM accounts WHERE id = account_id);
+        IF account_balance < money_amount THEN
+            RAISE NOTICE 'Insufficient balance to withdraw %', money_amount;
+            RETURN;
+        ELSE
+            UPDATE accounts SET balance = balance - money_amount WHERE id = account_id;
+        END IF;
+    END IF;
+    COMMIT;
+END
+$$
+    LANGUAGE plpgsql;
