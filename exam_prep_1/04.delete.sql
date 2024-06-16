990488f0487b23e3f3499d0ac271c86217eab409
vatari <majestic13@gmail.com>
2024-06-16 09:25:40 +0300

    exam prep 04

---
+++ exam_prep_1/04.delete.sql
@@ -0,0 +1,7 @@
+DELETE
+FROM volunteers
+WHERE department_id = (SELECT id FROM volunteers_departments WHERE department_name = 'Education program assistant');
+
+DELETE
+FROM volunteers_departments
+WHERE id = (SELECT id FROM volunteers_departments WHERE department_name = 'Education program assistant');
