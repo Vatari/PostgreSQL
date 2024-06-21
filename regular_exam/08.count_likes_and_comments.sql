ed8bac065bdfc2f495450cbbdc5779edc34c0711
vatari <majestic13@gmail.com>
2024-06-21 08:41:19 +0300

    regular exam task 08

---
+++ regular_exam/08.count_likes_and_comments.sql
@@ -0,0 +1,16 @@
+SELECT 
+    p.id AS photo_id,
+    COUNT(DISTINCT l.id) AS likes_count,
+    COUNT(DISTINCT c.id) AS comments_count
+FROM 
+    photos p
+LEFT JOIN 
+    likes l ON p.id = l.photo_id
+LEFT JOIN 
+    comments c ON p.id = c.photo_id
+GROUP BY 
+    p.id
+ORDER BY 
+    likes_count DESC, 
+    comments_count DESC, 
+    photo_id ASC;
